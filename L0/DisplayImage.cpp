2424abbfcba7699b0ed1f02c8adbbd880e3bdc99
SergioGabete <774631@unizar.es>
2022-02-11 18:28:37 +0100

    implementado lo de transponer la matriz con los punteros, indexer e iterador

--- L0/DisplayImage.cpp
+++ L0/DisplayImage.cpp
@@ -1,22 +1,51 @@
 #include <stdio.h>
 #include <opencv2/opencv.hpp>
+
 using namespace cv;
-int main(int argc, char** argv )
-{
-    if ( argc != 2 )
-    {
-        printf("usage: DisplayImage.out <Image_Path>\n");
-        return -1;
+// int main(int argc, char** argv )
+// {
+//     if ( argc != 2 )
+//     {
+//         printf("usage: DisplayImage.out <Image_Path>\n");
+//         return -1;
+//     }
+//     Mat image;
+//     image = imread( argv[1], 1 );
+//     if ( !image.data )
+//     {
+//         printf("No image data \n");
+//         return -1;
+//     }
+//     namedWindow("Display Image", WINDOW_AUTOSIZE );
+//     imshow("Display Image", image);
+//     waitKey(0);
+//     return 0;
+// }
+
+int main(int, char**) {
+    // open the first webcam plugged in the computer
+    cv::VideoCapture camera(CAP_V4L2);
+    if (!camera.isOpened()) {
+        std::cerr << "ERROR: Could not open camera" << std::endl;
+        return 1;
     }
-    Mat image;
-    image = imread( argv[1], 1 );
-    if ( !image.data )
-    {
-        printf("No image data \n");
-        return -1;
+
+    // create a window to display the images from the webcam
+    cv::namedWindow("Webcam", WINDOW_AUTOSIZE);
+
+    // this will contain the image from the webcam
+    cv::Mat frame;
+        
+    // capture the next frame from the webcam
+    camera >> frame;
+    
+    // display the frame until you press a key
+    while (1) {
+        // show the image on the window
+        cv::imshow("Webcam", frame);
+        // wait (10ms) for a key to be pressed
+        if (cv::waitKey(10) >= 0)
+            break;
     }
-    namedWindow("Display Image", WINDOW_AUTOSIZE );
-    imshow("Display Image", image);
-    waitKey(0);
     return 0;
 }
--- L0/matrix.cpp
+++ L0/matrix.cpp
@@ -1,4 +1,6 @@
 #include "opencv2/highgui/highgui.hpp"
+#include <iostream>
+#include <ctime> 
 using namespace cv;
 
 void salt(cv::Mat &image, int n) {
@@ -30,7 +32,7 @@ void colorReducePointers(cv::Mat &img, int div=64) {
 void colorReduceIterators(cv::Mat &image, int div=64) {
     Mat_<Vec3b>::iterator it = image.begin<Vec3b>();
     // iterator created at the begining of the image
-    Mat_<Vec3b>::iterator itend = imagen.end<Vec3b>();
+    Mat_<Vec3b>::iterator itend = image.end<Vec3b>();
     // iterador created at the end of the image
     for ( ; it!= itend; ++it) { // loop over all pixels
         (*it)[0]= (*it)[0]/div*div + div/2;
@@ -41,9 +43,44 @@ void colorReduceIterators(cv::Mat &image, int div=64) {
 
 int main( int argc, char** argv ) {
     Mat img = imread( argv[1], -1 );
+    //En img se guarda la matriz de los pixeles
     if( img.empty() ) return -1;
+    //Aqui se muestra la imagen
     namedWindow( "Example2", WINDOW_AUTOSIZE );
     imshow( "Example2", img );
+    //Se espera a que terminemos el programa
+    //Meter aqui lo de transformar matrices y comparar tiempos
+    Mat img1 = img.clone(),img2=img.clone(),img3=img.clone();
+    //Use functions from OpenCV
+    
+    //Traverse with pointers
+    auto t = std::chrono::high_resolution_clock::now();
+    colorReducePointers(img1);
+    auto d = std::chrono::high_resolution_clock::now()-t; 
+    std::cout <<"Tiempo de punteros: " << std::chrono::duration_cast<std::chrono::milliseconds>(d).count() << "ms"<<std::endl;
+    //Traverse with iterators
+    t = std::chrono::high_resolution_clock::now();
+    colorReduceIterators(img2);
+    d = std::chrono::high_resolution_clock::now()-t; 
+    std::cout <<"Tiempo de iterador: " << std::chrono::duration_cast<std::chrono::milliseconds>(d).count() << "ms"<<std::endl;
+    //Traverse with index
+    t = std::chrono::high_resolution_clock::now();
+    salt(img3,1000);
+    d = std::chrono::high_resolution_clock::now()-t; 
+    std::cout <<"Tiempo de indexer: " << std::chrono::duration_cast<std::chrono::milliseconds>(d).count() << "ms"<<std::endl;
+
+    namedWindow( "Punteros", WINDOW_AUTOSIZE );
+    imshow( "Punteros", img1 );
+    namedWindow( "Iterator", WINDOW_AUTOSIZE );
+    imshow( "Iterator", img2 );
+    namedWindow( "Indexer", WINDOW_AUTOSIZE );
+    imshow( "Indexer", img3 );
     waitKey( 0 );
     destroyWindow( "Example2" );
+    destroyWindow( "Punteros" );
+    destroyWindow( "Iterator" );
+    destroyWindow( "Indexer" );
+
+    //Analisis de tiempo de tranverse matrix
+
 }
