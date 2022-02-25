6f6f5790cff0c7b713dc79f299e3b23ec03fde39
aaronainzon17 <aaronainzon@gmail.com>
2022-02-25 14:10:48 +0100

    alien en un fichero aparte y main a medias

---
+++ L1/alienEffect.cpp
@@ -0,0 +1,72 @@
+#include "opencv2/opencv.hpp"
+#include "iostream"
+
+using namespace std;
+using namespace cv;
+
+Mat frame, mask;
+int slider_G, slider_R, slider_B;
+
+Mat getSkin(Mat input){
+    //Valores para detectar la piel
+    int Y_MIN = 0;
+    int Y_MAX = 255;
+    int Cr_MIN = 133;
+    int Cr_MAX = 173;
+    int Cb_MIN = 77;
+    int Cb_MAX = 127;
+
+    Mat skin;
+    //first convert our RGB image to YCrCb
+    cvtColor(input,skin,COLOR_BGR2YCrCb);
+
+    //uncomment the following line to see the image in YCrCb Color Space
+    //imshow("YCrCb Color Space",skin);
+
+    //filter the image in YCrCb color space
+    inRange(skin,Scalar(Y_MIN,Cr_MIN,Cb_MIN),Scalar(Y_MAX,Cr_MAX,Cb_MAX),skin);
+
+    return skin;
+}
+
+void on_trackbar(int, void*) {
+   add(frame, Scalar(slider_B,slider_G,slider_R), frame, mask);
+}
+
+int main(){
+    VideoCapture camera(0);
+    if (!camera.isOpened()) {
+        cerr << "ERROR: Could not open camera" << endl;
+        return 1;
+    }
+
+    // create a window to display the images from the webcam
+    //namedWindow("Webcam", WINDOW_AUTOSIZE);
+    namedWindow("Alien", WINDOW_AUTOSIZE); // Create Window
+    
+    slider_B = 0;
+    slider_G = 255;
+    slider_R = 0;
+
+    createTrackbar( "Blue", "Alien", &slider_B, 255, on_trackbar );
+    createTrackbar( "Green", "Alien", &slider_G, 255, on_trackbar );
+    createTrackbar( "Red", "Alien", &slider_R, 255, on_trackbar );
+   
+    //start an infinite loop where webcam feed is copied to cameraFeed matrix
+    //all of our operations will be performed within this loop
+    while(1) {
+        // capture the next frame from the webcam
+        camera >> frame;
+
+        //show the current image
+        imshow("Original Image",frame);
+
+        mask = getSkin(frame);
+        //frame.setTo(Scalar(0,255,0), mask);
+        //add(frame, Scalar(0,255,0), frame, mask);
+        on_trackbar(1,0);
+        imshow("Alien",frame);
+        waitKey(30);
+    }
+    return 0;
+}
--- L1/main.cpp
+++ L1/main.cpp
@@ -6,13 +6,8 @@
 using namespace std;
 using namespace cv;
 
-// static void on_trackbar( int, void* )
-// {
-//    alpha = (double) alpha_slider/alpha_slider_max ;
-//    beta = ( 1.0 - alpha );
-//    addWeighted( src1, alpha, src2, beta, 0.0, dst);
-//    imshow( "Linear Blend", dst );
-// }
+Mat frame, mask;
+int slider_G, slider_R, slider_B;
 
 void equalize(Mat img, float a, float b){
     Mat img_aux = img.clone();
@@ -38,54 +33,33 @@ void atomaticEqualize(Mat img){
 }
 
 
-//Deteccion de piel
+/*
+* getSkin: Funcion que detecta la piel
+*/
 Mat getSkin(Mat input){
     //Valores para detectar la piel
-    int Y_MIN;
-    int Y_MAX;
-    int Cr_MIN;
-    int Cr_MAX;
-    int Cb_MIN;
-    int Cb_MAX;
-    Y_MIN  = 0;
-    Y_MAX  = 255;
-    Cr_MIN = 133;
-    Cr_MAX = 173;
-    Cb_MIN = 77;
-    Cb_MAX = 127;
+    int Y_MIN = 0;
+    int Y_MAX = 255;
+    int Cr_MIN = 133;
+    int Cr_MAX = 173;
+    int Cb_MIN = 77;
+    int Cb_MAX = 127;
 
     Mat skin;
     //first convert our RGB image to YCrCb
     cvtColor(input,skin,COLOR_BGR2YCrCb);
 
     //uncomment the following line to see the image in YCrCb Color Space
-    //cv::imshow("YCrCb Color Space",skin);
+    //imshow("YCrCb Color Space",skin);
 
     //filter the image in YCrCb color space
     inRange(skin,Scalar(Y_MIN,Cr_MIN,Cb_MIN),Scalar(Y_MAX,Cr_MAX,Cb_MAX),skin);
-    cout<<"skin es"<<skin<<endl;
+
     return skin;
+}
 
-    // //Convertir imagen en hsv
-    // //Si un pixel es 255 lo pongo verde
-    // for (int i=0; i<input.rows; i++) {
-    //     uchar* data= input.ptr<uchar>(i); // pointer to row i
-    //     uchar* dataMask= skin.ptr<uchar>(i);
-    //     for (int j=0; j<input.cols*input.channels(); j++) {
-            
-    //         if(dataMask[j] == 255){
-    //             //data[j]
-    //         }
-    //     }
-    // }
-    Mat dst;
-    cvtColor(input,input,COLOR_BGR2YCrCb);
-    add(input, Scalar(100, 0, 0), dst, skin);
-    namedWindow("alien", WINDOW_NORMAL);
-    imshow("alien", dst);
-    waitKey(0);
-    
-
+void on_trackbar_ALIEN(int, void*) {
+   add(frame, Scalar(slider_B,slider_G,slider_R), frame, mask);
 }
 
 
@@ -97,13 +71,8 @@ int main(int, char**) {
         return 1;
     }
 
-    // this will contain the image from the webcam
-    Mat img;
-        
-    // capture the next frame from the webcam
-    camera >> img;
-    Mat imgAColor = img.clone();
-    cvtColor( img, img, COLOR_BGR2GRAY );
+    //Mat imgAColor = frame.clone();
+    //cvtColor( frame, frame, COLOR_BGR2GRAY );
     cout << "Que efecto quieres aplicar ?" << endl;
     cout << " 1: Contraste \n 2: Alien \n 3: Poster \n 4: Distorsion" << endl;
     int val;
@@ -111,16 +80,39 @@ int main(int, char**) {
     
     if (val == 1){
         cout << "Aplicando efecto de CONTRASTE" << endl;
-        //equalize(img,1.5,10);
-        //equalize(img,2,20);
-        //equalize(img,100,100);
-        atomaticEqualize(img);
+        //equalize(frame,1.5,10);
+        //equalize(frame,2,20);
+        //equalize(frame,100,100);
+        atomaticEqualize(frame);
     }else if (val == 2) {
         cout << "Aplicando efecto de ALIEN" << endl;
-        Mat alienImage = getSkin(imgAColor);
-        namedWindow("alien", WINDOW_NORMAL);
-        imshow("alien", alienImage);
-        waitKey(0);
+        namedWindow("Alien", WINDOW_AUTOSIZE); // Create Window
+    
+        slider_B = 0;
+        slider_G = 255;
+        slider_R = 0;
+
+        createTrackbar( "Blue", "Alien", &slider_B, 255, on_trackbar_ALIEN );
+        createTrackbar( "Green", "Alien", &slider_G, 255, on_trackbar_ALIEN );
+        createTrackbar( "Red", "Alien", &slider_R, 255, on_trackbar_ALIEN );
+
+        while(1) {
+            // capture the next frame from the webcam
+            camera >> frame;
+
+            //show the current image
+            imshow("Original Image",frame);
+
+            mask = getSkin(frame);
+            //frame.setTo(Scalar(0,255,0), mask);
+            //add(frame, Scalar(0,255,0), frame, mask);
+            on_trackbar_ALIEN(1,0);
+            imshow("Alien",frame);
+            if (waitKey(10) >= 0)
+            break;
+        }
+        return 0;
+   
     }else if (val == 3){
         cout << "Aplicando efecto de POSTER" << endl;
     }else if (val == 4) {
@@ -128,15 +120,7 @@ int main(int, char**) {
     }
     
     
-    // display the frame until you press a key
-    //while (1) {
-    //    // show the image on the window
-    //    imshow("Webcam", img);
-    //    // wait (10ms) for a key to be pressed
-    //    if (waitKey(10) >= 0)
-    //        break;
-    //}
-    return 0;
+   
 }
 
 /*
