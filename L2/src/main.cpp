bdf721e241abcec5abe7d5375b2bac742aa17b56
aaronainzon17 <aaronainzon@gmail.com>
2022-03-16 17:11:38 +0100

    sadv

--- L2/src/main.cpp
+++ L2/src/main.cpp
@@ -11,7 +11,7 @@ using namespace cv;
 /*Implementacion de la matriz del kernel a mano y tras eso utilizar el la func filter2D para aplicar el gradiente*/
 
 Mat hough(Mat img, Mat angle, Mat magnitude);
-
+double normalizar_pi_pi(double th);
 int main(int, char**) {
 
     // Reading image
@@ -37,14 +37,15 @@ Luego para mostrar solo puedes mostrar de 0,255 por tanto hay que pasarla a 8UC1
 Para implementar sobel hay que escalarlo porque sino se te va de valores (o algo así ha dicho rosario)
 */
     
-    Mat kernelY = (Mat_<float>(3,3)<<1.,2.,1.,0.,0.,0.,-1.,-2.,-1.); //kernel para el eje y
-    Mat kernelX = (Mat_<float>(3,3)<<-1.,0.,1.,-2.,0.,2.,-1.,0.,1.); //kernel para el eje X
+    Mat kernelY = (Mat_<double>(3,3)<<1.,2.,1.,0.,0.,0.,-1.,-2.,-1.); //kernel para el eje y
+    Mat kernelX = (Mat_<double>(3,3)<<-1.,0.,1.,-2.,0.,2.,-1.,0.,1.); //kernel para el eje X
     
     Mat sobelX,sobelY,sobelXaux,sobelYaux;
-
+    //Normalizar el sobelX e Y 
     filter2D(img_blur,sobelX,CV_64F,kernelX);
     filter2D(img_blur,sobelY,CV_64F,kernelY);
-
+    sobelX = sobelX/4.0;
+    sobelY = sobelY/4.0;
     //Esto es solo para mostrarlo, los calculos de los gradientes se hacen con los valores originales
     sobelXaux = sobelX/2 + 128;
     sobelXaux.convertTo(sobelXaux,CV_8UC1);
@@ -65,6 +66,8 @@ Para implementar sobel hay que escalarlo porque sino se te va de valores (o algo
     magnitude.convertTo(magnitude, CV_8UC1);
     imshow("modulo", magnitude);
     cout << magnitude << endl;
+    //threshold(magnitude, magnitude_aux, 30, 255, THRESH_BINARY);
+    //imshow("modulo_th", magnitude_aux);
     waitKey(0);
 
     angle_aux = ((angle/CV_PI) * 128);
@@ -73,7 +76,7 @@ Para implementar sobel hay que escalarlo porque sino se te va de valores (o algo
     waitKey(0);
     
     //destroyAllWindows();
-    float th;
+    double th;
     int x,y;
     //Apartado 2
     //cout << angle << endl;
@@ -88,7 +91,7 @@ Para implementar sobel hay que escalarlo porque sino se te va de valores (o algo
 }
 
 Mat hough(Mat img, Mat angle, Mat magnitude){
-    float threshold = 1000;
+    double threshold = 30;
     cout << "La imagen tiene " << img.rows << " filas" << endl;
     int centro[img.cols];
     //Se inicializa el vector a 0
@@ -99,34 +102,33 @@ Mat hough(Mat img, Mat angle, Mat magnitude){
     int entran = 0;
     for (int i = 0; i < img.rows; i++){
         for(int j = 0; j < img.cols; j++){
-            if (magnitude.at<float>(i,j) >= threshold){
-                cout << "La celda " << i << "," << j << " tiene el valor " << magnitude.at<float>(i,j) << endl;
-                float x = j - img.cols/2;
-                float y = img.rows/2 - i;
-                float th = angle.at<float>(i,j);
-                float p = x * cos(th) + y * sin(th); // rho = distancia al punto de origen 
+            if (magnitude.at<double>(i,j) >= threshold){
+                //cout << "La celda " << i << "," << j << " tiene el valor " << magnitude.at<double>(i,j) << endl;
+                double x = j - img.cols/2;
+                double y = img.rows/2 - i;
+                double th = angle.at<double>(i,j);
+                double p = x * cos(th) + y * sin(th); // rho = distancia al punto de origen 
 
-                float distance_90 = th - (CV_PI/2);
-                float distance_270 = th - ((3*CV_PI)/2); 
-                // float distance_90 = abs(th - (CV_PI/2));
-                // float distance_270 = abs(th - ((3*CV_PI)/2));
-                // float distance_180 = abs(th - (CV_PI));
-                // float distance = abs(th);
+                double distance_90 = abs(normalizar_pi_pi(th - (CV_PI/2)));
+                double distance_270 = abs(normalizar_pi_pi(th - ((3*CV_PI)/2))); 
+                // double distance_90 = abs(th - (CV_PI/2));
+                // double distance_270 = abs(th - ((3*CV_PI)/2));
+                double distance_180 = abs(normalizar_pi_pi(th - (CV_PI)));
+                double distance_360 = abs(normalizar_pi_pi(th - (2*CV_PI)));
+                double distance = abs(th);
                 //if (dist > 0.07){
                 //if ((distance_90 > 0.088) && (distance_270 > 0.088)){
-                if ((distance_90 > 0.088) && (distance_270 > 0.088)){   //&& (distance_180 > 0.088) && (distance > 0.088)
+                if ((distance_90 > 0.088) && (distance_270 > 0.088) && (distance > 0.088) && (distance_180 > 0.088) && (distance_360 > 0.088)){   //&& (distance_180 > 0.088) && (distance > 0.088)
                     entran++;
                     //Vote Line 
                     int x_fuga = (p - y*sin(th)) / cos(th);		// Se calcula la x sabiendo que y = 0 y conociendo rho(p)
                     //int x_fuga = (p) / cos(th);
 
-                    if (x_fuga < img.cols/2 && x_fuga >= -img.cols/2) {	// Se comprueba que corta en la imagen.
+                    if ((x_fuga < img.cols/2) && (x_fuga >= -img.cols/2)) {	// Se comprueba que corta en la imagen.
                         x_fuga += img.cols/2;		// Se pone el corte en el rango.
                         centro[x_fuga]++;	// Se actualiza el valor.
-                    }
-                    
+                    }   
                 }
-                
             }
         }
     }
@@ -135,7 +137,7 @@ Mat hough(Mat img, Mat angle, Mat magnitude){
     //Se saca el punto mas votado
 	for(int i = 0; i < img.cols; i++){
 		if(centro[i] > centro[max_votos]){
-			cout << "La celda " << i << " tiene " << centro[i] << " votos" << endl;
+			//cout << "La celda " << i << " tiene " << centro[i] << " votos" << endl;
             max_votos = i;
 		}
 	}
@@ -147,20 +149,34 @@ Mat hough(Mat img, Mat angle, Mat magnitude){
 }
 
 void findMax(Mat magnitude) {
-    float mid = magnitude.rows / 2;
+    double mid = magnitude.rows / 2;
     cout << "La fila de la mitad es: " << mid << endl;
 
-    float max = 0;
+    double max = 0;
     for (int i = 0; i < magnitude.rows; i++){
         for(int j = 0; j < magnitude.cols; j++){
-            if (magnitude.at<float>(i,j) >= max){
-                max = magnitude.at<float>(i,j);
+            if (magnitude.at<double>(i,j) >= max){
+                max = magnitude.at<double>(i,j);
             }
         }
     }
     cout << "El maximo valor de la imagen es: " << max << endl;
 }
-
+double normalizar_pi_pi(double th){
+    while (th > CV_PI){
+        //cout << "Primer " << th<< endl;
+            th = th - 2 * CV_PI;
+            //waitKey(0);
+    }
+    while (th < -CV_PI){
+        //cout << "Segundo " << th << endl;
+        th = th + 2 * CV_PI;
+        //waitKey(0);
+    }
+    //cout << "FIN" << endl;
+    return th;
+}
+        
 /*
 votan dos puntos que podrian pertenecer al gradiente (que podrian pertener a un gradiente votan)
 1 opcion: votan puntos a lineas y lineas a puntos de fuga
@@ -170,3 +186,5 @@ Para cada fila
         elegir el threshold: pues el maximo partido por 10, o la media
         si tenemos la x,y y th pues scas la p 
 */
+
+//Dibujar el pto que esta votando y el valor al que vota (solo los que entrewn al th )
